393ed0d07be9e19b399c9dd6bd9d0134fb5e369a
JiaPing <ping.jia@ele.me>
2017-11-03 16:24:41 +0800

    Adjust configuration

--- config/app.php
+++ config/app.php
@@ -64,8 +64,8 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Shanghai',
+//    'timezone' => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,8 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh-CN',
+//    'locale' => 'en',
 
     /*
     |--------------------------------------------------------------------------
